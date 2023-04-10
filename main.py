fdbaffba5854c429a7d8f0dfada92efe23316780
cang8 <0.gizemgunes@gmail.com>
2023-04-10 11:47:39 +0300

    detection white

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ while True:
     red_lower = np.array([136, 87, 111], np.uint8)
     red_upper = np.array([180, 255, 255], np.uint8)
 
+    white_lower = np.array([0, 0, 0], np.uint8)
+    white_upper = np.array([0, 0, 255], np.uint8)
+
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
+
+    white_mask = cv2.inRange(hsvFrame, white_lower, white_upper)
 
     kernel = np.ones((5,5), "uint8")
 
     red_mask = cv2.dilate(red_mask, kernel)
     res_red = cv2.bitwise_and(frame, frame, mask=red_mask)
+
+    white_mask = cv2.dilate(white_mask, kernel)
+    res_white = cv2.bitwise_and(frame, frame, mask=white_mask)
 
     contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -24,8 +32,17 @@ while True:
         area = cv2.contourArea(contour)
         if (area > 300) :
             x, y, w, h = cv2.boundingRect(contour)
-            frame = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
-            cv2.putText(frame, "Red", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,0,0), 2)
+            frame = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+            cv2.putText(frame, "Red", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2)
+
+    contours, hierarchy = cv2.findContours(white_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours) :
+        area = cv2.contourArea(contour)
+        if (area > 300) :
+            x, y, w, h = cv2.boundingRect(contour)
+            frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+            cv2.putText(frame, "White", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,0,0), 2)
 
     cv2.imshow("detectionRed",frame)
 
